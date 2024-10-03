b75bc6b3c687d0a3cb8ed38586641cddf9c0dac1
RocuroniumMan <mattembretsen@gmail.com>
2024-10-03 13:52:32 -0700

    working on multiplate

--- multiPlate.py
+++ multiPlate.py
@@ -5,14 +5,13 @@ import pandas as pd
 #returns 2d array containing the data per plate
 def getPosData(numberPlates, filePath):
     allPosData = []
-    i = 1
     with open(filePath, 'r') as file:
         lines = file.readlines()
         for i in range(numberPlates):
             storePosData = []
             for line in lines: 
                 line = line.strip()
-                if line.startswith(f"Unk_{i}"):
+                if line.startswith(f"Unk_{i+1}"):
                     segment = line.split('\t')
                     position = segment[1]
                     value = segment[2]
@@ -23,4 +22,29 @@ def getPosData(numberPlates, filePath):
 
 
 def convert2xls(allPosData, fileName):
-    pass
+    for plate in allPosData:
+        plateData = []
+        for vec in plate:
+            position = vec[0][0]
+            data = vec[0][1]
+            alpha = ""
+            number = ""
+
+            for char in position:
+                if char.isalpha():
+                    alpha += char
+                elif char.isdigit():
+                    number += char
+                
+            data = {
+                'data': data,
+                'y' : alpha,
+                'x' :number
+            }   
+
+            plateData.append(data)
+        
+        df = pd.DataFrame(plateData)
+        df['data'] = pd.to_numeric(df['data']) #make sure that excel recognizes the data as a number not a string
+        pivotdf = df.pivot(index='y', columns='x', values='data') #had to rearrange to get proper format
+        pivotdf.to_excel(f'{fileName}.xlsx', index=False)  #can only be xlsx, xls engine is missing?
